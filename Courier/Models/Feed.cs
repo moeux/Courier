793621fc9ca540f151079b1169792f469ba26bc9
moeux <68925026+moeux@users.noreply.github.com>
2026-02-20 17:06:41 +0100

    fix: Change default interval to 60

--- Courier/Models/Feed.cs
+++ Courier/Models/Feed.cs
@@ -5,6 +5,6 @@ public class Feed
     public required string Name { get; set; }
     public required string Uri { get; set; }
     public required ulong ChannelId { get; set; }
-    public int Interval { get; set; } = 1;
+    public int Interval { get; set; } = 60;
     public DateTimeOffset LastUpdate { get; set; } = DateTimeOffset.UnixEpoch;
 }
