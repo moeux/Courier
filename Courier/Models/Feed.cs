eba718d77b4e92a6a726fd73e28b7f77936ea1ee
moeux <68925026+moeux@users.noreply.github.com>
2026-02-25 23:57:18 +0100

    feat!: Added equality and overrides

--- Courier/Models/Feed.cs
+++ Courier/Models/Feed.cs
@@ -4,9 +4,22 @@ public class Feed
 {
     public const long DefaultInterval = 60;
 
-    public required string Name { get; set; }
-    public required string Uri { get; set; }
-    public required ulong ChannelId { get; set; }
-    public long Interval { get; set; } = DefaultInterval;
+    public required string Name { get; init; }
+    public required string Uri { get; init; }
+    public required ulong ChannelId { get; init; }
+    public long Interval { get; init; } = DefaultInterval;
     public DateTimeOffset LastUpdate { get; set; } = DateTimeOffset.UnixEpoch;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj is not Feed other) return false;
+        return Name == other.Name && Uri == other.Uri && ChannelId == other.ChannelId;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Uri, ChannelId);
+    }
 }
